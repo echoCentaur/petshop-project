a511c1d55bd3b3b82b5a2a0d7d78c69341c611c6
echoCentaur <dr.shark123.al@gmail.com>
2026-01-19 22:41:27 +0100

    project created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import notFound from "./assets/img/02_404.png"
+
+function App(){
+    return (
+<div style={{textAlign:"center"}}>
+ <h1>Скоро тут будет проект</h1>
+    <img src={notFound} alt="not found"/>
+</div>
+    )
+}
+
+export default App
